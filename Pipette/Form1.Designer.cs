b58e6e97ef4e03b646410463421d5b525b3e6cff
anyhum97 <anyhum@yandex.ru>
2020-06-29 01:20:13 +0300

    Uploading

---
+++ Pipette/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Pipette
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox10 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(288, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(18, 16);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "R";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(309, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(54, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TabStop = false;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(309, 38);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(54, 20);
+			this.textBox2.TabIndex = 2;
+			this.textBox2.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(288, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(18, 16);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "G";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(309, 64);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(54, 20);
+			this.textBox3.TabIndex = 3;
+			this.textBox3.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(288, 66);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(17, 16);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "B";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(410, 38);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.Size = new System.Drawing.Size(54, 20);
+			this.textBox5.TabIndex = 5;
+			this.textBox5.TabStop = false;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(389, 40);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(17, 16);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Y";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(411, 12);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(54, 20);
+			this.textBox4.TabIndex = 4;
+			this.textBox4.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(389, 14);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(16, 16);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "X";
+			// 
+			// textBox10
+			// 
+			this.textBox10.Location = new System.Drawing.Point(410, 135);
+			this.textBox10.Name = "textBox10";
+			this.textBox10.ReadOnly = true;
+			this.textBox10.Size = new System.Drawing.Size(54, 20);
+			this.textBox10.TabIndex = 10;
+			this.textBox10.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(389, 137);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(17, 16);
+			this.label4.TabIndex = 21;
+			this.label4.Text = "Y";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(410, 109);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.ReadOnly = true;
+			this.textBox9.Size = new System.Drawing.Size(54, 20);
+			this.textBox9.TabIndex = 9;
+			this.textBox9.TabStop = false;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.Location = new System.Drawing.Point(389, 111);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(16, 16);
+			this.label7.TabIndex = 19;
+			this.label7.Text = "X";
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(309, 161);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.ReadOnly = true;
+			this.textBox8.Size = new System.Drawing.Size(54, 20);
+			this.textBox8.TabIndex = 8;
+			this.textBox8.TabStop = false;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.Location = new System.Drawing.Point(288, 163);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(17, 16);
+			this.label8.TabIndex = 17;
+			this.label8.Text = "B";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(309, 135);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.ReadOnly = true;
+			this.textBox7.Size = new System.Drawing.Size(54, 20);
+			this.textBox7.TabIndex = 7;
+			this.textBox7.TabStop = false;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label9.Location = new System.Drawing.Point(288, 137);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(18, 16);
+			this.label9.TabIndex = 15;
+			this.label9.Text = "G";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(309, 109);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.ReadOnly = true;
+			this.textBox6.Size = new System.Drawing.Size(54, 20);
+			this.textBox6.TabIndex = 6;
+			this.textBox6.TabStop = false;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label10.Location = new System.Drawing.Point(288, 111);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(18, 16);
+			this.label10.TabIndex = 13;
+			this.label10.Text = "R";
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 75;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(309, 225);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(155, 43);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Export";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(478, 281);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox10);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox9);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "Form1";
+			this.Text = "Pippet";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox10;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Pipette/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pipette
+{
+	public partial class Form1 : Form
+	{
+		Bitmap Canvas = new Bitmap(256, 256);
+		Bitmap Source = new Bitmap(32, 32);
+
+		private int dx = 0;
+		private int dy = 0;
+
+		private int xPos = 0;
+		private int yPos = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			KeyPreview = true;
+			TopMost = true;
+
+			KeyDown += Form1_KeyDown;
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch(e.KeyCode)
+			{
+				case Keys.ControlKey:
+				{
+					timer1.Enabled = !timer1.Enabled;
+					dx = 0;
+					dy = 0;
+					break;
+				}
+
+				case Keys.Left:
+				{
+					if(dx > -14)
+					{
+						dx = dx-1;
+					}
+					
+					DrawFrame();
+					break;
+				}
+
+				case Keys.Right:
+				{
+					if(dx < 14)
+					{
+						++dx;
+					}
+					
+					DrawFrame();
+					break;
+				}
+
+				case Keys.Up:
+				{
+					if(dy > -14)
+					{
+						--dy;
+					}
+
+					DrawFrame();
+					break;
+				}
+
+				case Keys.Down:
+				{
+					if(dy < 15)
+					{
+						++dy;
+					}
+
+					DrawFrame();
+					break;
+				}
+
+				case Keys.E:
+				{
+					Export();
+					break;
+				}
+
+				case Keys.Escape:
+				{
+					Close();
+					break;
+				}
+			}
+		}
+
+		private void DrawFrame()
+		{
+			Bitmap bitmap2 = (Bitmap)Canvas.Clone();
+
+			for(int i=128-10; i<128+2; ++i)
+			{
+				bitmap2.SetPixel(128-09+dx*8, i+dy*8, Color.Green);
+				bitmap2.SetPixel(128-10+dx*8, i+dy*8, Color.Green);
+				bitmap2.SetPixel(128-11+dx*8, i+dy*8, Color.Green);
+			}
+
+			for(int i=128-10; i<128+2; ++i)
+			{
+				bitmap2.SetPixel(128+0+dx*8, i+dy*8, Color.Green);
+				bitmap2.SetPixel(128+1+dx*8, i+dy*8, Color.Green);
+				bitmap2.SetPixel(128+2+dx*8, i+dy*8, Color.Green);
+			}
+
+			for(int i=128-10; i<128+2; ++i)
+			{
+				bitmap2.SetPixel(i+dx*8, 128-09+dy*8, Color.Green);
+				bitmap2.SetPixel(i+dx*8, 128-10+dy*8, Color.Green);
+				bitmap2.SetPixel(i+dx*8, 128-11+dy*8, Color.Green);
+			}
+
+			for(int i=128-10; i<128+2; ++i)
+			{
+				bitmap2.SetPixel(i+dx*8, 128+0+dy*8, Color.Green);
+				bitmap2.SetPixel(i+dx*8, 128+1+dy*8, Color.Green);
+				bitmap2.SetPixel(i+dx*8, 128+2+dy*8, Color.Green);
+			}
+
+			pictureBox1.Image = bitmap2;
+
+			Color color1 = Source.GetPixel(15+dx, 15+dy);
+			Color color2 = Source.GetPixel(16+dx, 15+dy);
+
+			textBox1.Text = color1.R.ToString();
+			textBox2.Text = color1.G.ToString();
+			textBox3.Text = color1.B.ToString();
+
+			textBox4.Text = (xPos + dx).ToString();
+			textBox5.Text = (yPos + dy).ToString();
+
+			textBox6.Text = color2.R.ToString();
+			textBox7.Text = color2.G.ToString();
+			textBox8.Text = color2.B.ToString();
+
+			textBox9.Text = (xPos + 1 + dx).ToString();
+			textBox10.Text = (yPos + dy).ToString();
+
+			GC.Collect();
+		}
+
+		private void UpdateImage()
+		{
+			int x = Cursor.Position.X;
+			int y = Cursor.Position.Y;
+
+			int width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size.Width;
+			int height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size.Height;
+
+			using(Graphics graphics = Graphics.FromImage(Source))
+			{
+				if(x - 16 > 0 && x + 16 < width && y - 16 > 0 && y + 16 < height)
+				{
+					graphics.CopyFromScreen(x - 15, y - 15, 0, 0, Source.Size);
+				}
+
+				for(int i=0; i<32; ++i)
+				{
+					for(int j=0; j<32; ++j)
+					{
+						Color color = Source.GetPixel(i, j);
+
+						for(int z=0; z<8; ++z)
+						{
+							for(int t=0; t<8; ++t)
+							{
+								Canvas.SetPixel(i*8+z, j*8+t, color);
+							}
+						}						
+					}
+				}
+
+				xPos = x;
+				yPos = y;
+
+				DrawFrame();
+
+				GC.Collect();
+			}
+		}
+
+		private void Export()
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+
+			stringBuilder.Append("\t\tprivate static bool Check()\r\n\t\t{\r\n");
+
+			stringBuilder.Append("\t\t\tPixels pixel = new Pixels\r\n\t\t\t{\r\n");
+			
+			stringBuilder.Append("\t\t\t\tColor1 = Color.FromArgb(");
+
+			stringBuilder.Append(textBox1.Text.ToString() + ", ");
+			stringBuilder.Append(textBox2.Text.ToString() + ", ");
+			stringBuilder.Append(textBox3.Text.ToString() + "),\r\n");
+
+			stringBuilder.Append("\t\t\t\tColor2 = Color.FromArgb(");
+
+			stringBuilder.Append(textBox6.Text.ToString() + ", ");
+			stringBuilder.Append(textBox7.Text.ToString() + ", ");
+			stringBuilder.Append(textBox8.Text.ToString() + ")\r\n");
+
+			stringBuilder.Append("\t\t\t};\r\n\r\n\t\t\treturn CheckSum.Check(");
+
+			stringBuilder.Append(textBox4.Text + ", " + textBox5.Text + ", ");
+
+			stringBuilder.Append("pixel, 1, 5);\r\n\t\t}\r\n");
+
+			Clipboard.SetText(stringBuilder.ToString());
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			UpdateImage();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Export();
+		}
+	}
+}
